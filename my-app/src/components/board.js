647abbe69b778a50546da8bc35ef99d705e9348a
Noa Vikman <noa.vikman@storytel.com>
2020-06-17 14:05:06 +0200

    refactored to functional components

--- my-app/src/components/board.js
+++ my-app/src/components/board.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../index.css';
 import { Square } from './square.js'
 import { SelectionSquare } from './selectionsquare.js'
@@ -6,149 +6,116 @@ import { calculateWinner } from './functions.js'
 import { calculateMove } from './functions.js'
 import { StartSquare } from './startsquare.js'
 
-export class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      gameReady: false,
-      gameStarted: false,
-      xEmoji: "",
-      oEmoji: "",
-      squares: Array(9).fill(null),
-      XIsNext: true,
-      Winner: null
-    };
-  }
+export function Board(props) {
+  const [gameReady, setGameReady] = useState(false);
+  const [gameStarted, setGameStarted] = useState(false);
+  const [xEmoji, setXEmoji] = useState("");
+  const [oEmoji, setOEmoji] = useState("");
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [XIsNext, setXIsNext] = useState(true);
+  const [Winner, SetWinner] = useState(null);
 
-  handleClick(i) {
-    if (!this.state.squares[i] && !this.state.Winner) {
-      const squares = this.state.squares.slice();
-      squares[i] = this.state.xEmoji;
-      const computerMove = calculateMove(squares,this.state.xEmoji,this.state.oEmoji);
-      squares[computerMove] = this.state.oEmoji;
-    
-      this.setState({
-        squares: squares,
-        XIsNext: this.state.XIsNext,
-        Winner: calculateWinner(squares)
-      });
+  function handleClick(i) {
+    if (!squares[i] && !Winner) {
+      const sq = squares.slice();
+      sq[i] = xEmoji;
+      const computerMove = calculateMove(sq,oEmoji,xEmoji);
+      sq[computerMove] = oEmoji;
+      setSquares(sq);
+      setXIsNext(true)
+      SetWinner(calculateWinner(sq))
     }
   }
 
-  selClick(i, n) {
-    if (n === 1 && !(this.state.oEmoji === i)) {
-      this.setState({
-        xEmoji: i,
-        oEmoji: this.state.oEmoji,
-        gameReady: Boolean(i && this.state.oEmoji)
-      });
+  function selClick(i, n) {
+    if (n === 1 && !(oEmoji === i)) {
+      setXEmoji(i);
+      setGameReady(Boolean(i && oEmoji))
     }
-    if (n === 2 && !(this.state.xEmoji === i)) {
-      this.setState({
-        xEmoji: this.state.xEmoji,
-        oEmoji: i,
-        gameReady: Boolean(i && this.state.xEmoji)
-      });
+    if (n === 2 && !(xEmoji === i)) {
+      setOEmoji(i)
+      setGameReady(Boolean(i && xEmoji))
     }
   }
 
-  startClick() {
-    if(!this.state.gameStarted){
-      this.setState({
-        gameStarted: true
-      })
+  function startClick() {
+    if(!gameStarted){
+      setGameStarted(true)
     } else {
-      this.setState({
-        gameStarted: false,
-        squares: Array(9).fill(null),
-        XIsNext: true,
-        Winner: null
-      });
+      setGameStarted(true)
+      setSquares(Array(9).fill(null))
+      setXIsNext(true)
+      SetWinner(null)
     }
-
   }
 
-  renderSquare(i) {
-    return <Square value={this.state.squares[i]}
-      onClick={() => this.handleClick(i)} />;
+  function renderSquare(i) {
+    return <Square value={squares[i]}
+      onClick={() => handleClick(i)} />;
   }
 
-  renderSelSquare(i, n) {
+  function renderSelSquare(i, n) {
     return <SelectionSquare value={i}
-      onClick={() => this.selClick(i, n)} />;
+      onClick={() => selClick(i, n)} />;
   }
 
-  renderStartSquare() {
-    var message = this.state.gameStarted? "Play again?" : "Start Game!"
+  function renderStartSquare() {
+    var message = gameStarted? "Play again?" : "Start Game!"
     return <StartSquare value={message}
-      onClick={() => this.startClick()} style={{ width: "1000px", fontSize: "10px" }} />
+      onClick={() => startClick()} style={{ width: "1000px", fontSize: "10px" }} />
   }
 
-  componentDidUpdate(prevState) {
-    // Typical usage (don't forget to compare props):
-    if (this.state.XIsNext) {
-      console.log("component updated")
-    }
+  if (!gameStarted) {
+    return (
+      <div className="main">
+        <div style={{marginBottom:"30px"}}>
+          Emoji Selection
+        </div>
+        <div className="board-row" style={{marginBottom:"30px"}}>
+          {renderSelSquare('😀', 1)}
+          {renderSelSquare('😈', 1)}
+          {renderSelSquare('💩', 1)}
+          {renderSelSquare('😀', 2)}
+          {renderSelSquare('😈', 2)}
+          {renderSelSquare('💩', 2)}
+        </div>
+        <div style={{marginBottom:"30px"}}>
+          Human {xEmoji.toString()}
+        </div>
+        <div>
+          Computer {oEmoji.toString()}
+        </div>
+        {gameReady && renderStartSquare()}
+      </div>
+    );
   }
-
-  
-
-  render() {
-    if (!this.state.gameStarted) {
-      return (
-        <div className="main">
-          <div style={{marginBottom:"30px"}}>
-            Emoji Selection
-          </div>
-          <div className="board-row" style={{marginBottom:"30px"}}>
-            {this.renderSelSquare('😀', 1)}
-            {this.renderSelSquare('😈', 1)}
-            {this.renderSelSquare('💩', 1)}
-            {this.renderSelSquare('😀', 2)}
-            {this.renderSelSquare('😈', 2)}
-            {this.renderSelSquare('💩', 2)}
-          </div>
-          <div style={{marginBottom:"30px"}}>
-            Human {this.state.xEmoji.toString()}
-          </div>
-          <div>
-            Computer {this.state.oEmoji.toString()}
-          </div>
-          {this.state.gameReady && this.renderStartSquare()}
+  else {
+    return (
+      <div className="main2">
+        <div className="status" style={{marginBottom:"30px", textAlign:"center"}}>{'TIC TAC TOE'}</div>
+        <div className="board-row">
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
         </div>
-      );
-    }
-    else {
-      // const status = 'Next move: ' + (this.state.XIsNext ? this.state.xEmoji : this.state.oEmoji)
-      return (
-        <div className="main2">
-          {/* <div className="status" style={{marginBottom:"30px", paddingTop:"80px"}}>{status}</div> */}
-          <div className="status" style={{marginBottom:"30px", textAlign:"center"}}>{'TIC TAC TOE'}</div>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board-row" style={{marginBottom:"30px"}}>
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
-          <div style= {{textAlign:"center"}}>
-            {Boolean(this.state.Winner) && this.state.Winner!=="draw" && "The winner is "+this.state.Winner}
-            {Boolean(this.state.Winner) && this.state.Winner=="draw" && "It is a "+this.state.Winner}
-          </div>
-          <div> 
-            {Boolean(this.state.Winner) && this.renderStartSquare()}
-          </div>
-          
+        <div className="board-row">
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
         </div>
-      );
-    }
+        <div className="board-row" style={{marginBottom:"30px"}}>
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
+        </div>
+        <div style= {{textAlign:"center"}}>
+          {Boolean(Winner) && Winner!=="draw" && "The winner is "+Winner}
+          {Boolean(Winner) && Winner=="draw" && "It is a "+Winner}
+        </div>
+        <div> 
+          {Boolean(Winner) && renderStartSquare()}
+        </div>
+      </div>
+    );
   }
 }
--- my-app/src/components/game.js
+++ my-app/src/components/game.js
@@ -3,14 +3,12 @@ import '../index.css';
 import {Board} from './board.js'
 
 
-export class Game extends React.Component {
-  render() {
-      return(
-        <div className="game">
-          <div className="game-board">
-            <Board />  
-          </div>  
-        </div>
-      )
-  }
+export function Game (props) {
+    return(
+    <div className="game">
+        <div className="game-board">
+        <Board />  
+        </div>  
+    </div>
+    )
 }
